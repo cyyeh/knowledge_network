4b216657c6b7e36d8bf7f6c35e5da2a965f9a4c5
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-01-11 12:28:28 +0800

    update current status

--- generate_graph_config.py
+++ generate_graph_config.py
@@ -4,8 +4,6 @@ import os
 dotIO_posts_path = "/Users/chihyu/Desktop/TYCS/dotIO/source/_posts"
 directory = os.listdir(dotIO_posts_path)
 
-
-
 for f in directory:
     if f.endswith(".md"):  # check if it's real blog post file
        print(f) 
