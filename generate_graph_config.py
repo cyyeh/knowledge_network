24122b2e1b20326960d88bc0172e8a951d3b9576
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-01-11 13:18:23 +0800

    finish adding json data

--- generate_graph_config.py
+++ generate_graph_config.py
@@ -1,9 +1,41 @@
 # parse blog posts locally to generate network nodes and edges and save it to a json file
 import os
+import json
 
-dotIO_posts_path = "/Users/chihyu/Desktop/TYCS/dotIO/source/_posts"
+dotIO_posts_path = "/Users/chihyu/Desktop/TYCS/dotIO/source/_posts/"
 directory = os.listdir(dotIO_posts_path)
+total_analysis = {}
 
+# parse categories and tags inside each blog post
 for f in directory:
     if f.endswith(".md"):  # check if it's real blog post file
-       print(f) 
+        with open(dotIO_posts_path + f, mode='r') as file_descriptor:
+            lines = file_descriptor.read().splitlines()
+            analysis = {
+                "tags": [],
+                "categories": []
+            }
+            filter_tags_set = {'in process', 'finished', 'waited'}
+            read_tags_flag = False
+            read_categories_flag = False
+            for line in lines:
+                if "tags:" in line:
+                    read_tags_flag = True
+                    read_categories_flag = False
+                elif "categories:" in line:
+                    read_categories_flag = True
+                    read_tags_flag = False
+                elif "description:" in line:
+                    break
+                else:
+                    content = line.split("- ")[-1]
+                    if read_tags_flag and content not in filter_tags_set:
+                        analysis["tags"].append(content)
+                    elif read_categories_flag:
+                        analysis["categories"].append(content)
+            total_analysis[f] = analysis
+
+# convert dictionary type to json data
+# save json data to docs/
+with open("docs/total_analysis.json", mode="w") as f:
+    json.dump(total_analysis, f)
