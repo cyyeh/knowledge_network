9cf5a65a142beeda5eeab9305ac38d045e9bbf4b
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-01-14 12:06:02 +0800

    update json data file

--- auto_build.sh
+++ auto_build.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
-if [[ -n "$1" ]]; then
+commit_message = $1
+if [[ -n "$commit_message" ]]; then
     python generate_graph_config.py && git add . && git commit -m $1 && git push
 else
-    python generate_graph_config.py && git add . && git commit -m "update json data" && git push
+    python generate_graph_config.py && git add . && git commit -m "update json data file" && git push
 fi
