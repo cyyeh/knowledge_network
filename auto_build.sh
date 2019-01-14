573bbf4ad49b35f0ef41b0e95f283ffc3bf5a6cd
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-01-14 12:18:12 +0800

    update total_analysis.json

--- auto_build.sh
+++ auto_build.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-if [ "$1" == 0 ]; then
-    python generate_graph_config.py && git add . && git commit -m "update json data file" && git push
+if [ "$1" == "" ]; then
+    python generate_graph_config.py && git add . && git commit -m "update total_analysis.json" && git push
 else
     python generate_graph_config.py && git add . && git commit -m "$1" && git push
 fi
