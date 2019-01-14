6732fe1726a4b5ecbbe85045614fdc31efe3f8a8
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-01-14 12:17:25 +0800

    reorganize index js file

--- auto_build.sh
+++ auto_build.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
-commit_message = $1
-if [[ -n "$commit_message" ]]; then
-    python generate_graph_config.py && git add . && git commit -m "$1" && git push
-else
+if [ "$1" == 0 ]; then
     python generate_graph_config.py && git add . && git commit -m "update json data file" && git push
+else
+    python generate_graph_config.py && git add . && git commit -m "$1" && git push
 fi
